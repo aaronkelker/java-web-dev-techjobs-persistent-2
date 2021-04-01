a8926e57098f9f8bf4359e8eeba18263d97eab25
Aaron Kelker <aaronkelker@gmail.com>
2021-04-01 17:37:57 -0500

    first commit

--- queries.sql
+++ queries.sql
@@ -1,7 +1,12 @@
 ## Part 1: Test it with SQL
-
+next_val bigint
+employer varchar
+id int
+name varchar
+skills varchar
 ## Part 2: Test it with SQL
 
 ## Part 3: Test it with SQL
+Truncate Table
 
 ## Part 4: Test it with SQL
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,33 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @NotNull
+    @Size(min = 3, max = 500, message ="Input must be between 3 and 500 characters")
+    private String description;
+
+    @ManyToMany(mappedBy="skills")
+    private List<Job> jobs = new ArrayList<Job>();
+
+    public Skill() {}
+
+    public Skill(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
