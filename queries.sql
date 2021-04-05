3a019e15ebd988e9a059e44dbeb96497f56e4427
aaronkelker <74572370+aaronkelker@users.noreply.github.com>
2021-04-05 16:07:05 -0500

    updated queries.sql

--- queries.sql
+++ queries.sql
@@ -14,3 +14,7 @@ WHERE location = 'St. Louis City'
 Truncate Table
 
 ## Part 4: Test it with SQL
+SELECT DISTINCT name, description
+FROM skill
+INNER JOIN job_skills ON skill.id = job_skills.skills_id
+ORDER BY name;
